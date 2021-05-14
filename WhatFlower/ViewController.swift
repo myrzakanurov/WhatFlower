2adc556f99ec62960e4846fa239ef907488cd60c
Myrzakanurov Arman <myrzakanurov.a@gmail.com>
2021-05-14 23:20:54 +0600

    Completed Version

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,14 +6,101 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    let flowerManager = FlowerManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerShop().model) else {
+            fatalError("Cannot import model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image")
+            }
+            
+            self.navigationItem.title = classification.identifier.capitalized
+            
+            self.performRequest(flowerName: classification.identifier)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
+    func performRequest(flowerName: String) {
+        
+        let wikipediaURL = "https://en.wikipedia.org/w/api.php"
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts|pageimages",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+            "pithumsize" : "500"
+        ]
+        
+        AF.request(wikipediaURL, parameters: parameters).responseJSON { response in
+            if response.error != nil {
+                print(response.error!.errorDescription!)
+                return
+            }
+            
+            let json: JSON = JSON(response.value!)
+            
+            let pageId = json["query"]["pageids"][0].stringValue
+            let flowerDescription = json["query"]["pages"][pageId]["extract"].stringValue
+            let flowerImageURL = json["query"]["pages"][pageId]["thumbnail"]["source"].stringValue
+            
+            self.label.text = flowerDescription
+            self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
